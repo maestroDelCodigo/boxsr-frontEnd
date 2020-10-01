41183238832c2a02d11502585f4154435a06fe7c
Francisco Jaime <fjjaime42@gmail.com>
2020-10-01 13:44:29 +0200

    Creados componentes y rutas.

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,8 +2,38 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminComponent } from './admin.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { ColeccionesComponent } from './pages/colecciones/colecciones.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
+import { PedidosComponent } from './pages/pedidos/pedidos.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+  },
+  {
+    path: 'clientes',
+    component: ClientesComponent,
+  },
+  {
+    path: 'colecciones',
+    component: ColeccionesComponent,
+  },
+  {
+    path: 'inventario',
+    component: InventarioComponent,
+  },
+  {
+    path: 'pedidos',
+    component: PedidosComponent,
+  },
+  {
+    path: 'productos',
+    component: ProductosComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { HeaderAdminComponent } from './components/header-admin/header-admin.component';
+import { PedidosComponent } from './pages/pedidos/pedidos.component';
+import { ColeccionesComponent } from './pages/colecciones/colecciones.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [
+    AdminComponent,
+    HeaderAdminComponent,
+    PedidosComponent,
+    ColeccionesComponent,
+    ProductosComponent,
+    InventarioComponent,
+    ClientesComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
