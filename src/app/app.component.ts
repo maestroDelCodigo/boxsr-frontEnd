9c8329d7a1aec2d0ffe3f3b9f9418d5a93f2244c
Francisco Jaime <fjjaime42@gmail.com>
2020-10-01 11:09:07 +0200

    Añadida logica para ocultar el header si estas en admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router , NavigationEnd  } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { AuthService } from './core/security-services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'boxsr-front';
-
+  displayHeader: boolean;
   displayLogin = false;
+
+  constructor(private router: Router, private route: ActivatedRoute){
+    router.events.pipe(
+      filter((event) => event instanceof NavigationEnd))
+      .subscribe(x => {
+
+            const url =  x['url'] as string;
+
+            if (url.indexOf('admin') > 0){
+              this.displayHeader = false;
+            }
+            else{
+              this.displayHeader = true;
+            }
+
+          }
+      );
+  }
+
+  ngOnInit(): void {
+  }
+
 
   mostrarLogin(display: boolean): void{
     this.displayLogin = display;
