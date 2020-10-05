ab617c7c8bf510241ecff27bb2539fa5c1103242
noemi <noesanz__7@msn.com>
2020-10-05 18:39:37 +0200

    page envio-devolucion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { PielComponent } from './pages/sobre-boxsr/piel/piel.component';
 import { IngredientesComponent } from './pages/sobre-boxsr/ingredientes/ingredientes.component';
 import { AvisoLegalComponent } from './pages/avisoLegal/avisoLegal.component';
 import { PagoSeguroComponent } from './pages/pago-seguro/pago-seguro.component';
+import { EnviosDevolucionesComponent } from './pages/envios-devoluciones/envios-devoluciones.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -22,6 +23,7 @@ const routes: Routes = [
   { path: 'ingredientes', component: IngredientesComponent },
   { path: 'aviso-legal', component:AvisoLegalComponent },
   { path: 'pago-seguro', component:PagoSeguroComponent },
+  { path: 'envios-devoluciones', component:EnviosDevolucionesComponent },
   { path: 'admin', canActivate: [AuthGuardService], loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: '**', redirectTo: 'home' },
 ];
